6e7812c0fafa817cdde9e28ee268b7dd798bef83
Alliumcepa <fffonion@gmail.com>
2017-12-12 19:02:16 -0500

    continuous demo

--- demo.py
+++ demo.py
@@ -7,25 +7,30 @@ import numpy as np
 from scipy import misc
 
 TMPDIR = '.tmp-demo'
-import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-    if os.path.exists(TMPDIR):
-        shutil.rmtree(TMPDIR)
-    os.system("python ./generate.py 1 -l 4 -p binary_only -s %s" % TMPDIR)
-    img = os.listdir(TMPDIR)[0]
-    if os.system("imgcat %s 2>/dev/null" % os.path.join(TMPDIR, img)) > 0:
-        print("generated captcha saved at %s" % os.path.join(TMPDIR, img))
-        f = misc.imread(os.path.join(TMPDIR, img))
-        plt.imshow(f)
-        plt.show()
-    # load processed data
-    X = np.load('X.npy')
-    prediction = ''
+    clf = [joblib.load("digit%dmodel.out" % i) for i in range(4)]
 
-    for i in range(4):
-        tmp_clf = joblib.load("digit%dmodel.out" % (i))
-        prediction += str(tmp_clf.predict(X)[0])
+    print("model loaded")
 
-    print "Prediction result is: ", prediction
+    while True:
+        raw_input()
+        if os.path.exists(TMPDIR):
+            shutil.rmtree(TMPDIR)
+        os.system("python ./generate.py 1 -l 4 -p binary_only -s %s >/dev/null" % TMPDIR)
+        img = os.listdir(TMPDIR)[0]
+        if os.system("./imgcat %s 2>/dev/null" % os.path.join(TMPDIR, img)) > 0:
+            print("generated captcha saved at %s" % os.path.join(TMPDIR, img))
+            import matplotlib.pyplot as plt
+            f = misc.imread(os.path.join(TMPDIR, img))
+            plt.imshow(f)
+            plt.show()
+        # load processed data
+        prediction = ''
+
+        X = np.load('X.npy')
+        for i in range(4):
+            prediction += str(clf[i].predict(X)[0])
+
+        print("Prediction result is: %s" % prediction)
